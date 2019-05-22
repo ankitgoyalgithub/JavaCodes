05897f49c7ff51686aa5149c0dc4d00753267490
Ankit Goyal <goyal.ankit.049@gmail.com>
2019-05-22 22:21:57 +0530

    Initial Commit

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("HelloWorld");
+
+        int a = 20;
+        int b = 30;
+
+        System.out.println(a+b);
+    }
+}
